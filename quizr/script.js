4206f20460d723f040d87cb8663b577320a6c6ba
Korneel Eeckhout <korneel@pienter.tech>
2020-12-17 09:36:19 +0100

    init

---
+++ quizr/script.js
@@ -0,0 +1,31 @@
+const questions = [{
+  question: 'Linus Torvalds created Linux and Git.',
+  correctAnswer: 'True',
+}, {
+  question: 'The programming language "Python" is based off a modified version of "JavaScript".',
+  correctAnswer: 'False',
+}, {
+  question: 'The logo for Snapchat is a Bell.',
+  correctAnswer: 'False',
+}, {
+  question: 'RAM stands for Random Access Memory.',
+  correctAnswer: 'True',
+}, {
+  question: '"HTML" stands for Hypertext Markup Language.',
+  correctAnswer: 'True',
+}, {
+  question: 'In most programming languages, the operator ++ is equivalent to the statement "+= 1".',
+  correctAnswer: 'True',
+}, {
+  question: 'The Windows 7 operating system has six main editions.',
+  correctAnswer: 'True',
+}, {
+  question: 'The Windows ME operating system was released in the year 2000.',
+  correctAnswer: 'True',
+}, {
+  question: 'The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.',
+  correctAnswer: 'False',
+}, {
+  question: 'Linux was first created as an alternative to Windows XP.',
+  correctAnswer: 'False',
+}];
